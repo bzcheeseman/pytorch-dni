bd92644f1f34eb00a4f31a8c8857760aff47837d
Aman <bzcheeseman@gmail.com>
2017-04-23 20:47:39 -0500

    training is not going well, my gpu is too small for this

--- mnist_test.py
+++ mnist_test.py
@@ -18,7 +18,7 @@ import numpy as np
 
 from tensorboard_logger import configure, log_value
 
-configure("training/cnndni_7")
+configure("training/cnndni_9_cuda_3")
 
 
 class MNISTExtractor(nn.Module):
@@ -30,17 +30,18 @@ class MNISTExtractor(nn.Module):
         self.FE1 = CNNDNI(nn.Sequential(
             nn.Conv2d(1, 16, 5),
             nn.BatchNorm2d(16),
-            nn.ReLU()
+            nn.ReLU(),
+            nn.MaxPool2d(2, stride=2)
         ), update_out_filters=16, labels=True, update=self.update)
         self.FE2 = CNNDNI(nn.Sequential(
-            nn.Conv2d(16, 32, 3, stride=2),
+            nn.Conv2d(16, 32, 3),
             nn.BatchNorm2d(32),
             nn.Tanh()
         ), update_out_filters=32, labels=True, update=self.update)
 
-        self.num_outs = int(32 * (np.floor((((28 - 5) + 1) - 3)/2) + 1)**2)
+        self.num_outs = int(32 * ((np.floor(((28 - 5) + 1)/2) - 3) + 1)**2)
         self.classifier = DNI(nn.Linear(self.num_outs, 10), update_num_out=10, update=False)
-        self.class_opt = self.classifier.init_optimizer(optim.Adam, {'lr': 0.001, 'weight_decay': 1e-7})
+        self.class_opt = self.classifier.init_optimizer(optim.Adam, {'lr': 0.001})
 
     def set_update_false(self):
         self.update = False
@@ -55,7 +56,7 @@ class MNISTExtractor(nn.Module):
         x = self.FE2(x, labels)  # update 2
 
         if self.update:
-            self.FE1.synthetic_update(self.FE2.update_grad)  # update 3
+            self.FE1.synthetic_update(self.FE2.update_grad.cuda())  # update 3
 
         x = x.view(x.size(0), -1)
         x = self.classifier(x)  # return the output
@@ -66,14 +67,16 @@ class MNISTExtractor(nn.Module):
         l.backward()
         self.class_opt.step()  # update 4
 
-        self.FE2.synthetic_update(dni_mne.classifier.update_grad)  # update 5
+        self.FE2.synthetic_update(self.classifier.update_grad.cuda())  # update 5
 
         return l
 
 if __name__ == "__main__":
     transform = tv.transforms.ToTensor()
 
-    batch = 50
+    log_file = open("training_log.txt", 'w+')
+
+    batch = 13
     synth_grad_update = 1
 
     train = tv.datasets.MNIST("data", train=True, transform=transform, download=False)
@@ -83,11 +86,12 @@ if __name__ == "__main__":
     test_loader = DataLoader(test, batch_size=batch, num_workers=4)
 
     dni_mne = MNISTExtractor()
+    dni_mne.cuda()
     criterion = nn.CrossEntropyLoss()
 
-    print_steps = 10
+    print_steps = 100
 
-    for epoch in range(5):
+    for epoch in range(1000):  # 52 epochs at batch size 250
         running_loss = 0.0
 
         for i, data in enumerate(train_loader, 0):
@@ -96,20 +100,25 @@ if __name__ == "__main__":
             labels = Variable(labels)
 
             for j in range(synth_grad_update):
-                outputs = dni_mne(inputs, labels)
+                outputs = dni_mne(inputs.cuda(), labels.cuda())
 
-            l = dni_mne.optimizer_step(outputs, labels, criterion)
+            l = dni_mne.optimizer_step(outputs, labels.cuda(), criterion)
 
             running_loss += l.data[0]
 
             if i % print_steps == print_steps-1:
                 current_step = i + 1 + len(train_loader) * epoch
-                print("Current step: ", current_step, "Loss: ", running_loss / print_steps)
-                log_value("Loss", running_loss / print_steps, step=current_step)
-
+                log_file.write("Current step: {} Loss: {}\n".format(current_step, running_loss / print_steps))
+                # log_value("Loss", running_loss / print_steps, step=current_step)
                 running_loss = 0.0
 
+        torch.save(dni_mne.state_dict(), "models/mnist_model_ckpt_{}.dat".format(epoch))
+
     print("Training Finished")
+    torch.save(dni_mne.state_dict(), "models/mnist_model.dat")
+    # import os
+    # os.remove("models/mnist_model_ckpt*")
+
     dni_mne.set_update_false()
     total_loss = 0.0
     correct = 0
@@ -118,9 +127,9 @@ if __name__ == "__main__":
         inputs = Variable(inputs)
         labels = Variable(labels)
 
-        outputs = dni_mne(inputs)
+        outputs = dni_mne(inputs.cuda())
         # outputs = classifier(features)
-        l = criterion(outputs, labels)
+        l = criterion(outputs, labels.cuda())
         total_loss += l.data[0]
 
         _, max_index = np.where(outputs.cpu().data.numpy() == np.max(outputs.cpu().data.numpy()))
